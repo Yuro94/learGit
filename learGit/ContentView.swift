2c0f893058ecd0b47e7d14a029f1a92eea519f0f
YurikMnatsakanyan <Yurik@fhhealth.com>
2022-05-26 23:14:40 +0400

    Initial Commit

---
+++ learGit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learGit
+//
+//  Created by Yurik Mnatsakanyan on 26.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learGit/learGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  learGitApp.swift
+//  learGit
+//
+//  Created by Yurik Mnatsakanyan on 26.05.22.
+//
+
+import SwiftUI
+
+@main
+struct learGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
